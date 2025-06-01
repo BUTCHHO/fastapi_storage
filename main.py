cb0dfffdc07ec4c1e1e00e4ef04aac3deccae6a3
butcho <skeletorkoltmm@gmail.com>
2025-06-02 01:46:28 +0300

    unused view class remvoed from main.py

--- main.py
+++ main.py
@@ -55,6 +55,8 @@ def download_entity_endpoint(entity_path_in_storage: str, background_tasks: Back
     background_tasks.add_task(archivator.cleanup_temp_files)
     return response
 
+    #TODO отправляет пустые файлы. Надо исправить
+
 
 
 @app.post('/upload-entity')
@@ -68,52 +70,5 @@ async def upload_entity_endpoint(path: str, files: list[UploadFile]):
         await storage_writer.async_write_from_fastapi_uploadfile_to_file(file, fpath)
 
 
-class LoadEntityEndpoints:
-    def __init__(self): pass
-
-    @staticmethod
-    @app.get('/download-entity')
-    def download_entity_endpoint(entity_path_in_storage: str, background_tasks: BackgroundTasks):
-        absolute_entity_path = storage_reader.join_with_root_path(entity_path_in_storage)
-        try:
-            raise_if_path_invalid(absolute_entity_path)
-        except PathGoesBeyondLimits:
-            raise HTTPException(status_code=403, detail={"message": f'path {entity_path_in_storage} goes beyond limits', "code": 'path_goes_beyond_limits'})
-        except EntityDoesNotExists:
-            raise HTTPException(status_code=404, detail={"message": f'entity at {entity_path_in_storage} does not exists', "code": 'entity_does_not_exists'})
-
-
-        if storage_reader.is_dir(entity_path_in_storage):
-            zip_path = archivator.create_large_zip(absolute_entity_path)
-            background_tasks.add_task(archivator.cleanup_temp_files, zip_path)
-
-            return FileResponse(
-                zip_path,
-                filename=f"{zip_path.name}",
-                media_type="application/zip"
-            )
-        elif storage_reader.is_file(entity_path_in_storage):
-            entity_name = storage_reader.get_name(entity_path_in_storage)
-            encoded_entity_name = quote(entity_name)
-            return FileResponse(
-                absolute_entity_path,
-                filename=entity_name,
-                headers={"Content-Disposition": f"attachment; filename*=UTF-8''{encoded_entity_name}"},
-                content_disposition_type="attachment"
-            )
-
-    @staticmethod
-    @app.post('/upload-entity')
-    async def upload_entity_endpoint(path: str, files: list[UploadFile]):
-        abs_path = storage_reader.join_with_root_path(path)
-
-        def is_path_valid(a):
-            pass  # TODO затычка
-
-        if not is_path_valid(abs_path):
-            return HTTPException(400, {'message': 'invalid path'})
-        for file in files:
-            fpath = f'{abs_path}/{file.filename}'
-            await storage_writer.async_write_from_fastapi_uploadfile_to_file(file, fpath)
 
 
