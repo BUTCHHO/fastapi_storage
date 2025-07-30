6cb32c821cd38913af41d49b2f56ebe70984700c
BUTCHHO <skeletorkoltmm@gmail.com>
2025-07-30 20:18:55 +0300

    config class added

--- config.py
+++ config.py
@@ -3,19 +3,25 @@ from os import getenv
 
 load_dotenv('.env')
 
-STORAGE_PATH = getenv('STORAGE_PATH')
-DATABASE_URL = getenv('DATABASE_URL')
-SESSION_EXPIRE_TIME = getenv('SESSION_EXPIRE_TIME_DAYS')
-SESSION_COOKIES_EXPIRE_TIME = getenv('SESSION_COOKIES_EXPIRE_TIME_SECONDS')
-CACHE_HOST = getenv('CACHE_HOST')
-CACHE_PORT = int(getenv('CACHE_PORT'))
-CACHE_EXPIRE_TIME = getenv('CACHE_EXPIRE_TIME_SECONDS')
+class Config:
+    STORAGE_PATH = getenv('STORAGE_PATH')
+    TEST_STORAGE_PATH = getenv('GET_TEST_STORAGE')
+    DATABASE_URL = getenv('DATABASE_URL')
+    TEST_DATABASE_URL = getenv('TEST_DATABASE_URL')
+    SESSION_EXPIRE_TIME = getenv('SESSION_EXPIRE_TIME_DAYS')
+    SESSION_COOKIES_EXPIRE_TIME = getenv('SESSION_COOKIES_EXPIRE_TIME_SECONDS')
+    CACHE_HOST = getenv('CACHE_HOST')
+    CACHE_PORT = int(getenv('CACHE_PORT'))
+    CACHE_EXPIRE_TIME = getenv('CACHE_EXPIRE_TIME_SECONDS')
 
 
-assert STORAGE_PATH is not None
-assert DATABASE_URL is not None
-assert SESSION_EXPIRE_TIME is not None
-assert SESSION_COOKIES_EXPIRE_TIME is not None
-assert CACHE_PORT is not None
-assert CACHE_HOST is not None
-assert CACHE_EXPIRE_TIME is not None
+    assert STORAGE_PATH is not None
+    assert TEST_STORAGE_PATH is not None
+    assert TEST_DATABASE_URL is not None
+    assert DATABASE_URL is not None
+    assert SESSION_EXPIRE_TIME is not None
+    assert SESSION_COOKIES_EXPIRE_TIME is not None
+    assert CACHE_PORT is not None
+    assert CACHE_HOST is not None
+    assert CACHE_EXPIRE_TIME is not None
+
