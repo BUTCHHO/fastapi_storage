59f864d84f9b9fdbfd71a72b754881401170fce1
BUTCHHO <skeletorkoltmm@gmail.com>
2025-07-30 23:39:17 +0300

    Revert "Login class added"
    This reverts commit 415198260d511ecfcbd5e221d2f17db1b8f77ea9.
    
    'revert login class added"

---
+++ auth/logout_user.py
@@ -0,0 +1,18 @@
+class UserLogout:
+    def __init__(self, session_deleter):
+        self.session_deleter = session_deleter
+
+
+
+    async def logout_user(self, request, response):
+        """
+        :param response: something to get cookies from
+        :param request: something to put/delete cookies from
+        if your framework doesn't have something like this params then create class which wraps your framework's
+        cookies system and allows to use the call chain obj.cookies.get() & cookies.delete()
+        """
+        session_id = request.cookies.get('session_id')
+        if session_id is None:
+            return
+        response.delete_cookie('session_id')
+        await self.session_deleter.delete_session(session_id)
