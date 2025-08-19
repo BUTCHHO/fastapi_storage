ccc255799cf8043841797a5dfa279dc0c24f0490
BUTCHHO <skeletorkoltmm@gmail.com>
2025-08-19 21:57:25 +0300

    test delete entity added

--- tests/test_storage_writer.py
+++ tests/test_storage_writer.py
@@ -1,6 +1,7 @@
 import pytest
 
 from pathlib import Path
+import shutil
 
 from logic.storage_writer import StorageWriter
 from logic.storage_reader import StorageReader
@@ -28,18 +29,27 @@ def storage_reader(path_joiner, path_cutter):
 
 class TestStorageWriter:
 
-    @pytest.fixture(scope='class', autouse=True)
+    @pytest.fixture(autouse=True)
     def clear_dir_before_start(self):
         path_to_test_dir = Path(Config.DIR_FOR_TESTS)
         if path_to_test_dir.exists():
-            path_to_test_dir.rmdir()
+            shutil.rmtree(path_to_test_dir)
             path_to_test_dir.mkdir()
         else:
             path_to_test_dir.mkdir()
         yield
+
     def test_create_dir(self, storage_writer, storage_reader):
         name = 'test_dir'
         path = ''
         storage_writer.create_dir(name, path, exist_ok=True)
         is_exists = storage_reader.is_exists(name)
         assert is_exists == True
+
+    def test_delete_entity(self, storage_writer, storage_reader):
+        deletable_dir = Path(f'{Config.DIR_FOR_TESTS}/delete_me')
+        deletable_dir.mkdir()
+        assert storage_reader.is_exists(deletable_dir.__str__()) == True
+        storage_writer.delete_entity(deletable_dir.__str__())
+        assert storage_reader.is_exists(deletable_dir.__str__()) == False
+
